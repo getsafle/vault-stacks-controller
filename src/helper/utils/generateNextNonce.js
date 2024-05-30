7be98e6cf0561bff27bf2a1aec63045487b93133
SDargarh <sdargarh26@gmail.com>
2024-05-30 10:37:04 +0530

    Added hiro api key

--- src/helper/utils/generateNextNonce.js
+++ src/helper/utils/generateNextNonce.js
@@ -1,14 +1,17 @@
 const axios = require("axios");
 const { network: { MAINNET, TESTNET }} = require('../../config/index')
-const { QUICKNODE_BASE_URL, QUICKNODE_BASE_TEST_URL } = require('../../constants/index')
+const { HIRO_BASE_URL, HIRO_BASE_TEST_URL, HIRO_API_KEY } = require('../../constants/index')
 
 async function generateNextNonce(address, network) {
     try {
-        let URL = network === TESTNET ? QUICKNODE_BASE_TEST_URL : QUICKNODE_BASE_URL
+        let URL = network === TESTNET ? HIRO_BASE_TEST_URL : HIRO_BASE_URL
         URL = URL + `address/${address}/nonces`
         const nonce = await axios({
           url : `${URL}`,
-          method: 'GET'
+          method: 'GET',
+          headers: {
+            "x-hiro-api-key": HIRO_API_KEY,
+          },
         });
         return { nonce: BigInt(nonce.data.possible_next_nonce) }
       } catch (err) {
